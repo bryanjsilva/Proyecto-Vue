43af739b33f7f22c7946bc72c17eb257af9b8419
bryanjsilva <bryanzsilva@hotmail.com>
2021-05-31 17:45:23 -0500

    Se añade pantalla de login y firebase

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,74 @@
 <template>
-  <h1>Hello Vue!</h1>
+  <main class="container mt-5">
+    <h1 class="text-center text-light fw-bold">Mis gastos</h1>
+    <Login 
+    v-if='!login'
+    v-on:ingresoCorrecto='ingresoCorrecto'></Login>
+  </main>
 </template>
 
 <script>
+import firebase from 'firebase'
 
+import Login from './components/Login'
 export default {
   name: 'App',
   components: {
-    
+    Login
+  },
+  data: function(){
+    return{
+      login: false,
+      listaGastos: [],
+      gastos: {},
+      firebase: '',
+      db: ''
+    }
+  },
+  beforeMount(){
+      const firebaseConfig = {
+        apiKey: "AIzaSyCdIUSjkIUd49JoGW3YHZqNvbFkI7tPEc0",
+        authDomain: "proyecto-vue-bs-37e6f.firebaseapp.com",
+        projectId: "proyecto-vue-bs-37e6f",
+        storageBucket: "proyecto-vue-bs-37e6f.appspot.com",
+        messagingSenderId: "825583609859",
+        appId: "1:825583609859:web:dfadcb001efc73faff8636",
+        measurementId: "G-VZJWRW3RWT"
+      };
+
+      firebase.initializeApp(firebaseConfig);
+      this.db=firebase.firestore();
+      const settings = {timestampInSnapshots: true};
+      this.db.settings(settings);
+      this.firebase=firebase;
+
+      this.gastos = this.db.collection('usuarios');
+      this.gastos.get().then(gastos=>{
+        gastos.forEach(gasto=>{
+          this.listaGastos.push({
+            id: gasto.id,
+            nombre: gasto.data().nombre,
+            tipo: gasto.data().tipo,
+            monto: gasto.data().monto
+          })
+        })
+      })
+  },
+  methods:{
+    ingresoCorrecto(dato){
+      alert(dato)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Lato';
-}
+  body{
+    background: rgb(131,58,180);
+    background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(252,99,69,1) 100%);
+  }
+  #app {
+    font-family: 'Lato';
+    color: #6338FC;
+  }
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="row d-flex justify-content-center my-3">
+        <div class="col-11 col-md-4 border rounded px-3 bg-light">
+            <h2 class="text-center mt-3">Ingresar al sistema</h2>
+            <form class="my-3">
+                <div class="form-group">
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput">Correo electrónico</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <label for="floatingPassword">Contraseña</label>
+                    </div>
+                </div>
+                <button 
+                v-on:click.prevent='manejoClick($event)'
+                id='ingreso' 
+                class="btn btn-primary mx-auto mt-3 w-100 fw-bold">
+                    Ingresar &nbsp; <i class="fa fa-sign-in"></i>
+                </button>
+            </form>
+            <hr>
+            <div class="text-center">
+                <label for="registro" class="form-label">¿Eres nuevo?</label>
+                <button
+                v-on:click.prevent='manejoClick($event)' 
+                id='registro' 
+                class="btn btn-outline-primary fw-bold w-100 mt-2 mb-3">
+                    Regístrate &nbsp; <i class="fa fa-user"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    methods:{
+        manejoClick(evento){
+            if(evento.target.id==='ingreso'){
+                this.$emit('ingresoCorrecto','ingreso')
+            }
+            if(evento.target.id==='registro'){
+                this.$emit('ingresoCorrecto','registro')
+            }
+        }
+    }
+}
+</script>
